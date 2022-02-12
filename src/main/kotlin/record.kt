d5b83dae418ad0f2a58eb32be6eca14640d7d290
YasminMahad <yasminmahadabdullahi@gmail.com>
2022-02-12 10:30:22 +0300

    Assignment

---
+++ src/main/kotlin/record.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var student = "full_name"
+    println(student)
+    var age = 15
+    println(age)
+    var phoneNumber = 7234567643
+    println(phoneNumber)
+    var studentWeight : Float = 20.3F
+    println(studentWeight)
+    val kenya : Boolean = true
+    val others : Boolean = false
+    println(kenya)
+    println(others)
+
+}
